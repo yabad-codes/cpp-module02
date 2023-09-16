edc1b58ce468f1dd10b1fc254f5dfad299d49add
Yahya Abad <yabad@c1r6p2.1337.ma>
2023-09-16 11:18:31 +0100

    removing function methods comments

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,27 +6,19 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 15:38:24 by yabad             #+#    #+#             */
-/*   Updated: 2023/09/15 20:03:25 by yabad            ###   ########.fr       */
+/*   Updated: 2023/09/16 10:19:35 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Point::Point() : x(0), y(0) {
-	// std::cout << "Point Default constructor called" << std::endl;
-}
+Point::Point() : x(0), y(0) {}
 
-Point::Point(float const px, float const py) : x(px), y(py) {
-	// std::cout << "Point Float constructor called" << std::endl;
-}
+Point::Point(float const px, float const py) : x(px), y(py) {}
 
-Point::Point(const Point& other) : x(other.getX()), y(other.getY()) {
-	// std::cout << "Point Copy constructor called" << std::endl;
-}
+Point::Point(const Point& other) : x(other.getX()), y(other.getY()) {}
 
-Point::~Point() {
-	// std::cout << "Point Destructor called" << std::endl;
-}
+Point::~Point() {}
 
 Point&	Point::operator=(const Point& other) {
 	if (this == &other)
