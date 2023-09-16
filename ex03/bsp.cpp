11bfede48a7a7ebc209b67fee5fe68af8e824350
Yahya Abad <yabad@c1r6p2.1337.ma>
2023-09-16 11:32:38 +0100

    defining my own function to find square root using Newton-Raphson approximation

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,22 +6,33 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 15:14:48 by yabad             #+#    #+#             */
-/*   Updated: 2023/09/15 20:11:13 by yabad            ###   ########.fr       */
+/*   Updated: 2023/09/16 11:30:35 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+#define GUESS 100
+
+float my_sqrt(float x) {
+    if (x < 0)
+        return -1.0;
+    float guess = x / 2.0;
+    for (int i = 0; i < GUESS; i++) {
+        guess = (guess + x / guess) / 2.0; //Newton-Raphson method for approximation
+    }
+    return (guess);
+}
 
 Fixed	getSide(Point const a, Point const b) {
 	float	distance;
 
-	distance = sqrtf((b.getX().toFloat() - a.getX().toFloat()) * (b.getX().toFloat() - a.getX().toFloat()) + (b.getY().toFloat() - a.getY().toFloat()) * (b.getY().toFloat() - a.getY().toFloat()));
+	distance = my_sqrt((b.getX().toFloat() - a.getX().toFloat()) * (b.getX().toFloat() - a.getX().toFloat()) + (b.getY().toFloat() - a.getY().toFloat()) * (b.getY().toFloat() - a.getY().toFloat()));
 	return (Fixed(distance));
 }
 
 Fixed	getArea(Point const a, Point const b, Point const c) {
 	Fixed	p((getSide(a, b) + getSide(a, c) + getSide(b, c)) / 2);
-	Fixed	area(sqrtf(Fixed(p * (p - getSide(a, b)) * (p - getSide(b, c)) * (p - getSide(a, c))).toFloat()));
+	Fixed	area(my_sqrt(Fixed(p * (p - getSide(a, b)) * (p - getSide(b, c)) * (p - getSide(a, c))).toFloat()));
 	return (area);
 }
 
